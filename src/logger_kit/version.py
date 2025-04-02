9ae737b3f22090a1f6dc28dc8287a54a73f640a8
Rahmad Afandi <rahmadafandiii@gmail.com>
2025-04-02 17:38:49 +0700

    chore: update version to 0.0.3 and remove tomli dependency
    Update the project version to 0.0.3 and remove the tomli dependency from pyproject.toml. Simplify version.py by hardcoding version and metadata instead of parsing from pyproject.toml. Update poetry.lock to reflect dependency changes.

--- src/logger_kit/version.py
+++ src/logger_kit/version.py
@@ -1,13 +1,5 @@
-import tomli
-
-
-def get_pyproject():
-    with open("pyproject.toml", "rb") as f:
-        return tomli.load(f)
-
-
-__version__ = get_pyproject()["project"]["version"]
-__author__ = get_pyproject()["project"]["authors"][0]["name"]
-__author_email__ = get_pyproject()["project"]["authors"][0]["email"]
-__license__ = get_pyproject()["project"]["license"]
-__copyright__ = get_pyproject()["project"]["copyright"]
+__version__ = "0.0.3"
+__author__ = "Rahmad Afandi"
+__author_email__ = "rahmadafandiii@gmail.com"
+__license__ = "MIT"
+__copyright__ = "2025, Rahmad Afandi"
